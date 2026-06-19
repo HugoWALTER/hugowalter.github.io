0e4780cfa092b148f364638a00884f7d225e19e7
Hugo WALTER <hugo.walter@epitech.eu>
2026-06-19 11:04:25 +0200

    fix: responsive on horizontal devices, add: new icon for wind speed max and remove: °C

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network-first pour les APIs météo
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v5';
+const CACHE_NAME   = 'weatherradar-v6';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
