77077d11d8d75985df043d570c6de4616522c592
Hugo W <hugo.walter@epitech.eu>
2026-08-30 13:15:08 +0200

    fix: hPa Y axis display and tides card, update: fog, cloud and wind effect

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network-first pour les APIs météo
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v16';
+const CACHE_NAME   = 'weatherradar-v17';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
