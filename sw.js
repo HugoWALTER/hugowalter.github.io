d25d1558eb843aa07a3f5962afd918c07cb15f6a
Hugo WALTER <hugo.walter@epitech.eu>
2026-08-31 16:51:08 +0200

    fix: windgust by boosting size on mobile

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Network-first pour les APIs météo
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v18';
+const CACHE_NAME   = 'weatherradar-v19';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
