edb82709f74ee1a43eb11ca8e5224fb630d004dc
Hugo WALTER <hugo.walter@epitech.eu>
2026-09-10 09:42:15 +0200

    add: better performance, geoloc btn, share card button

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Cache-first pour les autres assets statiques
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v25';
+const CACHE_NAME   = 'weatherradar-v26';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
