966867a12262a1ac81357f06ba1cb5a8002f68ce
Hugo WALTER <hugo.walter@epitech.eu>
2026-09-12 11:54:14 +0200

    wip: bookmarks icons

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Cache-first pour les autres assets statiques
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v28';
+const CACHE_NAME   = 'weatherradar-v29';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
