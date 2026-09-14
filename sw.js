7dc09307df208a61171493617b44105d3e7a7dd8
Hugo WALTER <hugo.walter@epitech.eu>
2026-09-14 12:24:31 +0200

    add: storm severity card and snow-rain limit depending on height limit

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //             Cache-first pour les autres assets statiques
 // ──────────────────────────────────────────────────────────────────
 
-const CACHE_NAME   = 'weatherradar-v31';
+const CACHE_NAME   = 'weatherradar-v32';
 const OFFLINE_PAGE = '/';
 
 // Assets à mettre en cache immédiatement à l'installation
